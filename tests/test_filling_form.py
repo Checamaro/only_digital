84f089fa94c1e8f86433724e61be6deb8fc434a9
checamar0 <checamar02@gmail.com>
2024-01-29 10:06:36 +0300

    main

--- tests/test_filling_form.py
+++ tests/test_filling_form.py
@@ -1,18 +1,19 @@
 import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from pages.projects_brief_page import BriefPage
 
 
 
 @allure.description("Test filling form")
 def test_filling_in_form():
-
     options = webdriver.ChromeOptions()
-    # options.add_experimental_option("detach", True)
+    options.add_experimental_option("detach", True)
     options.add_argument('--headless')
-    g = Service('C:\\Users\\checa\\PycharmProjects\\pythonProject\\only_digital\\chromedriver.exe')
+    g = Service('C:/Users/checa/PycharmProjects/pythonProject/only_digital/chromedriver.exe')
     driver = webdriver.Chrome(options=options, service=g)
+
 
     print("Start Test")
 
